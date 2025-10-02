04716c03f59b113b1cb1a615d64c608b2b34b145
Alin Andrei Balasa <alin.andrei.balasa@blsalin.dev>
2025-10-02 10:49:03 +0300

    Moved more settings to general common

--- configuration/common-linux/configuration.nix
+++ configuration/common-linux/configuration.nix
@@ -6,52 +6,21 @@
 {
   imports =
     [
-        ../common/bundle.nix
+        ../common/configuration.nix
         ./modules/bundle.nix
     ];
-    
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_6_16;
 
-
-  networking.hostName = hostname; # Define your hostname.
-
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Set your time zone.
-  time.timeZone = "Europe/Bucharest";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_IE.UTF-8";
-    LC_IDENTIFICATION = "en_IE.UTF-8";
-    LC_MEASUREMENT = "en_IE.UTF-8";
-    LC_MONETARY = "en_IE.UTF-8";
-    LC_NAME = "en_IE.UTF-8";
-    LC_NUMERIC = "en_IE.UTF-8";
-    LC_PAPER = "en_IE.UTF-8";
-    LC_TELEPHONE = "en_IE.UTF-8";
-    LC_TIME = "en_IE.UTF-8";
-  };
 
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = stateVersion; # Did you read the comment?
 }
---
+++ configuration/common/configuration.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, lib, hostname, stateVersion, ... }:
+{
+  imports =
+    [
+        ./packages.nix
+    ];
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  networking.hostName = hostname; # Define your hostname.
+
+  # Set your time zone.
+  time.timeZone = "Europe/Bucharest";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_IE.UTF-8";
+    LC_IDENTIFICATION = "en_IE.UTF-8";
+    LC_MEASUREMENT = "en_IE.UTF-8";
+    LC_MONETARY = "en_IE.UTF-8";
+    LC_NAME = "en_IE.UTF-8";
+    LC_NUMERIC = "en_IE.UTF-8";
+    LC_PAPER = "en_IE.UTF-8";
+    LC_TELEPHONE = "en_IE.UTF-8";
+    LC_TIME = "en_IE.UTF-8";
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = stateVersion; # Did you read the comment?
+}
+    
