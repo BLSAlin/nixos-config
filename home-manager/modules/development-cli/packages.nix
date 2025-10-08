32c8e59448875aecc56aa798915f75eadd02ee2c
Alin Andrei Balasa <alin.andrei.balasa@blsalin.dev>
2025-10-08 14:36:28 +0300

    TMUX

--- home-manager/modules/development-cli/packages.nix
+++ home-manager/modules/development-cli/packages.nix
@@ -1,6 +1,32 @@
 {pkgs, ...}: {
   nixpkgs.config.allowUnfree = true;
 
+  programs.tmux = {
+    enable = true;
+    clock24 = true;
+    historyLimit = 5000;
+
+    newSession = true;
+
+    mouse = true;
+
+    keyMode = "vi";
+
+    extraConfig = ''
+      set -g default-terminal "xterm-256color"
+      set -ga terminal-overrides ",*256col*:Tc"
+      set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
+      set-environment -g COLORTERM "truecolor"
+
+      bind | split-window -h -c "#{pane_current_path}"
+      bind - split-window -v -c "#{pane_current_path}"
+    '';
+
+    plugins = with pkgs; [
+      tmuxPlugins.better-mouse-mode
+    ];
+  };
+
   home.packages = with pkgs; [
     # Development
     jdk17
