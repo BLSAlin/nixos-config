ac71663a7bcc2f351a9698dfc075e03029e7e4d6
Alin Andrei Balasa <alin.andrei.balasa@blsalin.dev>
2026-02-20 22:54:03 +0200

    Swapped for a daemon

--- configuration/hosts/bifrost/modules/homebrew.nix
+++ configuration/hosts/bifrost/modules/homebrew.nix
@@ -9,22 +9,22 @@
       "jellyfin"
     ];
 
-    launchd.user.agents.jellyfin = {
+    launchd.daemons.jellyfin = {
+      script = ''
+        # We use the full path to the internal binary of the Cask
+        exec /Applications/Jellyfin.app/Contents/MacOS/jellyfin \
+          --datadir "/Users/orc/.config/jellyfin/data" \
+          --cachedir "/Users/orc/.cache/jellyfin" \
+          --configdir "/Users/orc/.config/jellyfin" \
+          --logdir "/Users/orc/.config/jellyfin/log" \
+          --noninteractive
+      '';
       serviceConfig = {
         Label = "org.nixos.jellyfin";
-        # Using the standard Applications path for stability across version updates
-        ProgramArguments = [
-          "/Applications/Jellyfin.app/Contents/MacOS/jellyfin"
-          "--datadir" "/Users/orc/.config/jellyfin/data"
-          "--cachedir" "/Users/orc/.cache/jellyfin"
-          "--configdir" "/Users/orc/.config/jellyfin"
-          "--logdir" "/Users/orc/.config/jellyfin/log"
-          "--noninteractive" 
-        ];
+        # THIS IS THE KEY: It tells launchd to drop privileges to 'orc'
+        UserName = "orc";
         RunAtLoad = true;
         KeepAlive = true;
-        # Ensures the service waits for the network to be ready
-        ProcessType = "Background";
         StandardOutPath = "/Users/orc/.config/jellyfin/log/stdout.log";
         StandardErrorPath = "/Users/orc/.config/jellyfin/log/stderr.log";
       };
