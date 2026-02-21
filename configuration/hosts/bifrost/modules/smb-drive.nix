c18754f81f2a4196e56280923f1eb7342aa2256b
Alin Andrei Balasa <alin.andrei.balasa@blsalin.dev>
2026-02-22 01:24:35 +0200

    Samba share settings

--- configuration/hosts/bifrost/modules/smb-drive.nix
+++ configuration/hosts/bifrost/modules/smb-drive.nix
@@ -8,7 +8,7 @@ in
       # Source the credentials
       source /Users/orc/.smb_credentials
       
-      mount volume "smb://$username:$password@10.69.100.11/big-data"
+      mount -t smbfs volume "smb://$username:$password@10.69.100.11/big-data"
     '';
     
     serviceConfig = {
