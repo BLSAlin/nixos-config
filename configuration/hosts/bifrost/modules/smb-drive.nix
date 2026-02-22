4751a7aac570a4af442937af2432d177645bc9a9
Alin Andrei Balasa <alin.andrei.balasa@blsalin.dev>
2026-02-22 11:44:17 +0200

    Samba share settings

--- configuration/hosts/bifrost/modules/smb-drive.nix
+++ configuration/hosts/bifrost/modules/smb-drive.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 let 
   credentialsFilePath = "/Users/orc/.smb_credentials";
-  driveMountPoint = "/mnt/remoteBigData";
+  driveMountPoint = "/Volumes/remoteBigData";
 in 
 {
   launchd.daemons.mount-nas = {
