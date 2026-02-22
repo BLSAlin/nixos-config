bc0ec5470ffa2ae8f0b62a1d57ff5cdda997d8a6
Alin Andrei Balasa <alin.andrei.balasa@blsalin.dev>
2026-02-22 14:34:34 +0200

    Samba share settings

--- configuration/hosts/bifrost/modules/smb-drive.nix
+++ configuration/hosts/bifrost/modules/smb-drive.nix
@@ -13,7 +13,7 @@ in
       source ${credentialsFilePath}
       
       echo "Attempting mounting the SMB share under ${driveMountPoint}"
-      mount -t smbfs -o noatime,nobrowse,-d=777 smb://$username:$password@10.69.100.11/big-data ${driveMountPoint}
+      mount -t smbfs -o nosuid,noatime,nobrowse,-d=777 smb://$username:$password@10.69.100.11/big-data ${driveMountPoint}
     '';
     
     serviceConfig = {
