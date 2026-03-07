3c90bec238d4481adf134481b7010c0ccf58d4c9
Alin Andrei Balasa <alin.andrei.balasa@blsalin.dev>
2026-03-07 19:42:21 +0200

    Fish config upgrade

--- home-manager/modules/common/fish.nix
+++ home-manager/modules/common/fish.nix
@@ -1,29 +1,53 @@
-{config, ...}: {
+{lib, ...}: {
 
   programs.fish = {
     enable = true;
     generateCompletions = true;
 
     interactiveShellInit = ''
-    	set fish_greeting
+      set fish_greeting
+      fish_vi_key_bindings
     '';
-
 
     shellAbbrs =
       let
         flakeDir = "~/Projects/nix/nixos-config";
       in {
-
+        # Nix
         nxrb = "sudo nixos-rebuild switch --flake ${flakeDir}";
         update-flake = "nix flake update ${flakeDir}";
-
         hms = "home-manager switch --flake ${flakeDir}";
-#         pkgs = "nvim ${flakeDir}/nixos/packages";
 
+        # Git
+        gs = "git status";
+        ga = "git add";
+        gc = "git commit";
+        gp = "git push";
+        gpl = "git pull";
+        gd = "git diff";
+        gl = "git log --oneline --graph";
+        gco = "git checkout";
+        gb = "git branch";
+        gst = "git stash";
+
+        # Tool replacements
         vim = "nvim";
+        grep = "rg";
+        cat = "bat";
 
+        # General
         ll = "ls -alh";
         ff = "fastfetch";
       };
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  home.sessionVariables = {
+    EDITOR = lib.mkDefault "nvim";
+    VISUAL = lib.mkDefault "nvim";
   };
 }
