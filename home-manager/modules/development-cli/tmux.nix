13c28b67cf29f767cd4802dfcf9f4b9be4b7f804
Alin Andrei Balasa <alin.andrei.balasa@blsalin.dev>
2026-03-07 19:24:32 +0200

    TMux config - good state

--- home-manager/modules/development-cli/tmux.nix
+++ home-manager/modules/development-cli/tmux.nix
@@ -27,6 +27,12 @@
       set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
       set-environment -g COLORTERM "truecolor"
 
+      # Behavior
+      set -g renumber-windows on
+      set -g focus-events on
+      set -g monitor-activity on
+      set -g visual-activity off
+
       # Splits in current path
       bind | split-window -h -c "#{pane_current_path}"
       bind - split-window -v -c "#{pane_current_path}"
@@ -41,16 +47,27 @@
       bind -n S-Left swap-window -t -1\; select-window -t -1
       bind -n S-Right swap-window -t +1\; select-window -t +1
 
+      # Session switching
+      bind Tab choose-tree -s
+
       # Reload config
       bind r source-file ~/.config/tmux/tmux.conf \; display "Config reloaded"
 
+      # Pane status line (top of each pane)
+      set -g pane-border-status top
+      set -g pane-border-lines heavy
+      set -g pane-border-format " #[fg=colour250]#{pane_index}: #{pane_title}"
+      set -g pane-border-style "fg=#555555"
+      set -g pane-active-border-style "fg=#e5a00d"
+
       # Status bar
       set -g status-style "bg=#000000,fg=#d4d4d4"
-      set -g status-left " #S "
+      set -g status-left " #S#{?window_zoomed_flag, [Z],} "
       set -g status-right ""
       set -g window-status-format " #I:#W "
       set -g window-status-current-format " #I:#W "
       set -g window-status-current-style "fg=#000000,bg=#e5a00d,bold"
+      set -g window-status-activity-style "fg=#e5a00d,bg=#000000"
     '';
   };
 }
